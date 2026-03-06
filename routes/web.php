e5608b21602c96520f542abd2769e59b35e65c01
satokunDayo <satokun712@gmail.com>
2026-03-06 10:25:54 +0900

    Add search and wishlist routes

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,14 @@
+
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\GameSearchController;
+use App\Http\Controllers\WishlistController;
 
 Route::get('/', function () {
     return view('home');
 });
+Route::get("/search",[GameSearchController::class,"search"]);
+
+Route::post("/wishlist",[WishlistController::class,"store"])
+    ->middleware("auth");
